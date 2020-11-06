ebcce876fbd261f5bda8e4d34b071a7c76e80fbc
Martin Skatvedt <martskat@stud.ntnu.no>
2020-11-06 22:17:08 +0100

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React, { FC } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Login from "./Login";
-import Shop from "./Shop";
+import Login from "./components/Login";
+import Shop from "./components/Shop";
 
 const App: FC = () => (
   <Router>
--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 
 const Shop: FC = () => <h1> SHOP </h1>;
 
-export default Shop;
+export default styled(Shop)``;
