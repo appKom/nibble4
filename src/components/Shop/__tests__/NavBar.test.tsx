267c65b517feb1bdf3d52ef4bdb7befd7c07fe5e
Martin Skatvedt <martskat@stud.ntnu.no>
2021-01-17 23:49:03 +0100

    fixed typo in importing

--- src/components/Shop/__tests__/NavBar.test.tsx
+++ src/components/Shop/__tests__/NavBar.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import NavBar from "../NavBar";
+import Navbar from "../Navbar";
 
 afterEach(cleanup);
 it("renders", () => {
-  const { asFragment } = render(<NavBar />);
+  const { asFragment } = render(<Navbar />);
   expect(asFragment()).toMatchSnapshot();
 });
