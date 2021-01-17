2ce5b415f2cf30783a65810a7afd331695b1389c
Martin Skatvedt <martskat@stud.ntnu.no>
2021-01-17 23:00:52 +0100

    added integration test to basketWindow

--- src/components/Shop/BasketWindow/__tests__/index.test.tsx
+++ src/components/Shop/BasketWindow/__tests__/index.test.tsx
@@ -3,14 +3,24 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import BasketWindow from "../index";
 import { User } from "types/user";
-import {
-  StateProvider,
-  StateContext,
-  State,
-  StateContextProps,
-} from "state/state";
-import { CartItem } from "types/inventory";
+import { StateContext, State, StateContextProps } from "state/state";
+import { CartItem, createCartItem } from "types/inventory";
 import { modalTypes } from "types/modal";
+import { Product } from "types/inventory";
+
+const mockProduct: Product = {
+  pk: 95,
+  name: "Powerking",
+  description: "Energidrikk",
+  price: 10,
+  image: {
+    sm: "/media/images/responsive/sm/38688f77-21b6-4936-8ba6-c976538fb110.png",
+  },
+  category: {
+    pk: 3,
+    name: "Drikke",
+  },
+};
 
 const mockUser: User = {
   pk: 1337,
@@ -18,12 +28,9 @@ const mockUser: User = {
   first_name: "SuperAdmin",
 };
 
-const mockCartItem: Cart = {
-  object_id: 32,
-  quantity: 1,
-};
+const mockCartItem: CartItem = createCartItem(95);
 
-const mockState: State = {
+const mockStateWithUser: State = {
   user: mockUser,
   inventory: [],
   category: "Alt",
@@ -31,17 +38,44 @@ const mockState: State = {
   modalState: modalTypes.DISABLED,
 };
 
-const initialContext: StateContextProps = {
+const mockState: State = {
+  user: mockUser,
+  inventory: [mockProduct],
+  category: "Alt",
+  cart: { 95: mockCartItem },
+  modalState: modalTypes.DISABLED,
+};
+
+const userContext: StateContextProps = {
+  state: mockStateWithUser,
+  dispatch: () => null,
+};
+
+const fullContext: StateContextProps = {
   state: mockState,
   dispatch: () => null,
 };
 
 afterEach(cleanup);
-it("renders", () => {
-  const { asFragment } = render(
-    <StateContext.Provider value={initialContext}>
-      <BasketWindow />
-    </StateContext.Provider>
-  );
-  expect(asFragment()).toMatchSnapshot();
+
+describe("Unit-testing for BasketWindow", () => {
+  it("Should match snapshot", () => {
+    const { asFragment } = render(
+      <StateContext.Provider value={userContext}>
+        <BasketWindow />
+      </StateContext.Provider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("Integration test for BasketWindow with a BasketItem", () => {
+  it("Should render with an BasketItem", () => {
+    const { asFragment } = render(
+      <StateContext.Provider value={fullContext}>
+        <BasketWindow />
+      </StateContext.Provider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
