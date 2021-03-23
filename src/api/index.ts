54a66bc2ab2336b4caedafdf80e69726da8a1078
Martinskatvedt <martinskatvedt@gmail.com>
2021-03-23 17:50:51 +0100

    Sort inventory alphabeticly
    Co-authored-by: Petter Charles Redfern
    <petter.charles.redfern@online.ntnu.no>

--- src/api/index.ts
+++ src/api/index.ts
@@ -76,7 +76,7 @@ export const fetchInventory = async (url: string): Promise<Product[]> => {
   const response = await get({ url });
   if (response.ok) {
     const json = await response.json();
-    return json;
+    return json.sort((a: Product, b: Product) => (a.name > b.name ? 1 : -1));
   }
   return [];
 };
