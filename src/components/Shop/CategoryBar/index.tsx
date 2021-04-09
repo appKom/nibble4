1222920bdac5e0832fd3b3d296557214ecc6434e
Jorre <jorgenreim@gmail.com>
2021-04-09 14:04:14 +0200

    feature: automatically set category to favourite products on login for users who have bought 5 or more different products in the past

--- src/components/Shop/CategoryBar/index.tsx
+++ src/components/Shop/CategoryBar/index.tsx
@@ -2,16 +2,22 @@ import React, { useContext, FC } from "react";
 import styled from "styled-components";
 import CategoryButton from "./CategoryButton";
 import { StateContext } from "state/state";
-import { getCategories } from "types/inventory";
+import { favouritesCategory, getCategories } from "types/inventory";
+import { setCategory } from "../../../state/actions";
 
 const CategoryBar: FC = () => {
-  const { state } = useContext(StateContext);
+  const { state, dispatch } = useContext(StateContext);
 
   const categories = getCategories(state.inventory, state.user!);
 
   const CategoryBar = categories.map((category) => (
     <CategoryButton key={category} category={category} />
   ));
+
+  // If the logged in user has a substantial amount of products previously purchased,
+  // automatically set the category to "Dine favoritter".
+  if (state.user!.favourites.length > 5)
+    dispatch(setCategory(favouritesCategory));
 
   return <Container> {CategoryBar} </Container>;
 };
