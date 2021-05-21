3c95b38c02b7fc484a06bcac456efb0c5769e7e9
Martinskatvedt <martinskatvedt@gmail.com>
2021-05-21 10:05:44 +0200

    removed vscode error

--- src/api/index.ts
+++ src/api/index.ts
@@ -72,15 +72,3 @@ export const authorizedPost = ({
       Authorization: `Bearer ${loadToken()}`,
     },
   });
-<<<<<<< HEAD
-=======
-
-export const fetchInventory = async (url: string): Promise<Product[]> => {
-  const response = await get({ url }).catch(() => Promise.resolve(undefined));
-  if (response && response.ok) {
-    const json = await response.json();
-    return json.sort((a: Product, b: Product) => (a.name > b.name ? 1 : -1));
-  }
-  return [];
-};
->>>>>>> main
